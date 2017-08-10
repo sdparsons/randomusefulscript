b8e0d3687cef33fb0abb40f14eb589a03fa27b33
Sam Parsons <sam.parsons@psy.ox.ac.uk>
2017-08-10 10:10:51 +0100

    added scripts

--- rounding_functions.R
+++ rounding_functions.R
@@ -1,3 +1,5 @@
+# The idea is to remove leading zeros, while keeping the negative (if present), to a specified number of decimal places
+
 rounding.1dp <- function(val) { 
   sub("^(-?)0.", "\\1.", sprintf("%.1f", val)) 
 }
@@ -5,3 +7,18 @@ rounding.1dp <- function(val) {
 rounding.2dp <- function(val) { 
   sub("^(-?)0.", "\\1.", sprintf("%.2f", val)) 
 }
+
+
+M.SD.0dp <- function(val) {
+  paste(round(mean(val, na.rm = TRUE)), " (", 
+        round(sd(val, na.rm = TRUE)), ")", sep = "")}
+
+
+M.SD.1dp <- function(val) {
+  paste(sub("^(-?)0.", "\\1.", sprintf("%.1f", mean(val, na.rm = TRUE)))," (",
+        sub("^(-?)0.", "\\1.", sprintf("%.1f", sd(val, na.rm = TRUE))),")", sep = "")}
+
+
+M.SD.2dp <- function(val) {
+  paste(sub("^(-?)0.", "\\1.", sprintf("%.2f", mean(val, na.rm = TRUE)))," (",
+        sub("^(-?)0.", "\\1.", sprintf("%.2f", sd(val, na.rm = TRUE))),")", sep = "")}
